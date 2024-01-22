f30057c755a5c7e1de87c343429f804db20302c2
txxgio <156123419+txxgio@users.noreply.github.com>
2024-01-22 13:08:24 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function doSomething3 () {
+	
+}
+mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
+	
+})
 function doSomething () {
     highlight = sprites.create(img`
         1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
@@ -119,3 +125,61 @@ let mySprite5 = sprites.create(img`
     . . . f f 3 3 3 3 3 3 f f . . . 
     . . . . . f f f f f f . . . . . 
     `, SpriteKind.Player)
+tiles.placeOnTile(mySprite5, tiles.getTileLocation(1, 1))
+let mySprite6 = sprites.create(img`
+    . . . . . f f f f f f . . . . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . f f 3 3 3 3 3 3 3 3 f f . . 
+    . f f 3 3 3 3 3 3 3 3 3 3 f f . 
+    . f 3 3 3 3 3 3 3 3 3 1 3 3 f . 
+    f 3 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 1 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 1 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . f 3 3 3 3 3 3 3 3 3 3 3 f f . 
+    . . f 3 3 3 3 3 3 3 3 3 f f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . . . f f f f f f . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite6, tiles.getTileLocation(3, 1))
+let mySprite7 = sprites.create(img`
+    . . . . . f f f f f f . . . . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . f f 3 3 3 3 3 3 3 3 f f . . 
+    . f f 3 3 3 3 3 3 3 3 3 3 f f . 
+    . f 3 3 3 3 3 3 3 3 3 1 3 3 f . 
+    f 3 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 1 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 1 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . f 3 3 3 3 3 3 3 3 3 3 3 f f . 
+    . . f 3 3 3 3 3 3 3 3 3 f f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . . . f f f f f f . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite7, tiles.getTileLocation(5, 1))
+let sprite_8 = sprites.create(img`
+    . . . . . f f f f f f . . . . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . f f 3 3 3 3 3 3 3 3 f f . . 
+    . f f 3 3 3 3 3 3 3 3 3 3 f f . 
+    . f 3 3 3 3 3 3 3 3 3 1 3 3 f . 
+    f 3 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 1 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 1 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+    . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . f 3 3 3 3 3 3 3 3 3 3 3 f f . 
+    . . f 3 3 3 3 3 3 3 3 3 f f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . . . f f f f f f . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(sprite_8, tiles.getTileLocation(7, 1))
