e8bf8ab484303155e84e056e17b6fdc8934affd9
txxgio <156123419+txxgio@users.noreply.github.com>
2024-01-22 12:06:57 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,3 +3,21 @@ namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 128
     export const ARCADE_SCREEN_HEIGHT = 128
 }
+let mySprite = sprites.create(img`
+    . . . . . f f f f f f . . . . . 
+    . . . f f 2 2 2 2 2 2 f f . . . 
+    . . f f 2 2 2 2 2 2 2 2 f f . . 
+    . f f 2 2 2 2 2 2 2 2 2 2 f f . 
+    . f 2 2 2 2 2 2 2 2 2 1 2 2 f . 
+    f 2 2 2 2 2 2 2 2 2 1 1 1 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 1 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    . f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+    . f 2 2 2 2 2 2 2 2 2 2 2 f f . 
+    . . f 2 2 2 2 2 2 2 2 2 f f . . 
+    . . . f f 2 2 2 2 2 2 f f . . . 
+    . . . . . f f f f f f . . . . . 
+    `, SpriteKind.Player)
